3e89dfef3b6ae1a02a19811bad9d3760fa6dcdfe
Evgenii <evv13@pitt.edu>
2025-03-17 18:50:47 -0400

    relay

--- relay.py
+++ relay.py
@@ -3,8 +3,8 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2,GPIO.OUT)
-GPIO.output(2,GPIO.LOW)
-sleep(2)
-GPIO.output(2,GPIO.HIGH)
+GPIO.output(2,GPIO.LOW) # activate motor
+sleep(3)
+GPIO.output(2,GPIO.HIGH) # deactivate motor
 sleep(2)
 GPIO.cleanup()
