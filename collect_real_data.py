5d2c0e7805b0c08519de4aa3446d13acdddba451
EvgeniiVenediktov <evv13@pitt.edu>
2025-03-19 19:10:54 -0400

    time delay argument

--- collect_real_data.py
+++ collect_real_data.py
@@ -9,9 +9,12 @@ def save_image(img: ArrayLike, x:float, y:float, folder:str="data/real") -> None
     cv2.imwrite(folder+f"/x{x:.2f}_y{y:.2f}.jpg", img)
 
 
-def whole_process(controller, x, y):
+def whole_process(controller: MFController, x:float, y:float, time_delay=0.3, verbose=True) -> None:
     # Turn frame
     controller.set_tilt_y(y)
+
+    # Wait for system to set
+    time.sleep(time_delay)
 
     # Get image
     img = controller.capture_image()
@@ -21,7 +24,10 @@ def whole_process(controller, x, y):
 
     # Save
     save_image(img, x, y)
-    time.sleep(0.5)
+
+    # Log
+    if not verbose:
+        return
     print(controller.get_frame_position())
 
 
@@ -44,30 +50,32 @@ if __name__ == "__main__":
     Y_STOP = 0.06
 
 
-    x_tilt = np.arange(X_START, X_STOP, STEP)  # vertical tilt. Negative - looking down
-    y_tilt = np.arange(Y_START, Y_STOP, STEP)  # horizontal tilt. Negative - looking right
+    x_tilt = np.arange(X_START, X_STOP, STEP)  # horizontal tilt. Negative - looking right
+    y_tilt = np.arange(Y_START, Y_STOP, STEP)  # vertical tilt. Negative - looking up
 
-    print("len(x_tilt):", len(x_tilt))
+    if len(x_tilt) % 2 == 1:
+        X_STOP += STEP
+        x_tilt = np.arange(X_START, X_STOP, STEP)
     
 
     # Go through coordinate matrix
     i = 0
-
-
-
     while i in range(len(x_tilt)):
         x = x_tilt[i]
         controller.set_tilt_x(x)
+        # y: 1 -> 2 -> 3
         for y in y_tilt:
             whole_process(controller, x, y)
-
         i += 1
+
         x = x_tilt[i]
         controller.set_tilt_x(x)
+        # y: 3 -> 2 -> 1
         for y in y_tilt[::-1]:
             whole_process(controller, x, y)
         i += 1
     
+    # Return frame to (0, 0)
     controller.set_tilt_x(0)
     controller.set_tilt_y(0)
     print(controller.get_frame_position())
