b9480072bbd624548120a4b42957766972184374
Evgenii <evv13@pitt.edu>
2025-05-01 11:49:11 -0400

    test light

--- config.py
+++ config.py
@@ -8,12 +8,14 @@ REAL_DATA_COLLECTION_STEP = 0.01
 REAL_DATA_COLLECTION_DELAY = 0.1
 
 
-INFERENCE_MODEL_FILE_NAME = "001step_simplefc_400bs_0001lr_15sl_aug+_best_model.pth"
+INFERENCE_MODEL_FILE_NAME = "mixed_001step_simplefc_400bs_0001lr_15sl_aug+_best_model.pth"
 INFERENCE_MODEL_TYPE = "SimpleFC"
 
 TRAINING_IMAGE_RESOLUTION = (512, 512)
 
 LMDB_USE_COMPRESSION = False
 
-OPTIMUM_IMAGE_PATH = "/home/raspberry/projects/multireflection/data/real/x0.00_y0.00.jpg"
+OPTIMUM_IMAGE_PATH = "/home/raspberry/projects/multireflection/data/light/x0.00_y0.00.jpg"
+
+#OPTIMUM_IMAGE_PATH = "/home/raspberry/projects/multireflection/data/real/x0.00_y0.00.jpg"
 SIMILARITY_INDEX_THRESHOLD = 0.8
