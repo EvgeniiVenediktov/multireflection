880979ab4f77cbdca9abdcb0a80c40603547c367
Evgenii <evv13@pitt.edu>
2025-06-04 10:43:21 -0400

    new model data

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ REAL_DATA_COLLECTION_STEP = 0.01
 REAL_DATA_COLLECTION_DELAY = 0.1
 
 
-INFERENCE_MODEL_FILE_NAME = "004step_smallgrad_fullmix_512bs_00001lr_aug+_best_model.pth"
-#INFERENCE_MODEL_FILE_NAME = "001step_simplefc_400bs_0001lr_15sl_aug+_best_model.pth"
+INFERENCE_MODEL_FILE_NAME = "transfer_002step_smallgrad_fullmixdirtynewdark_512bs_00001lr_aug+_best_model.pth"
 INFERENCE_MODEL_TYPE = "GradientSimpleFC"
 
 TRAINING_IMAGE_RESOLUTION = (512, 512)
@@ -22,7 +21,7 @@ LMDB_USE_COMPRESSION = False
 
 # OPTIMUM_IMAGE_PATH = "/home/raspberry/projects/multireflection/data/real/x0.00_y0.00.jpg"
 SIMILARITY_INDEX_THRESHOLD = 0.94
-OPTIMUM_IMAGE_PATH_LIST = ["/home/raspberry/projects/multireflection/data/real/x0.00_y0.00.jpg", "/home/raspberry/projects/multireflection/data/light/x0.00_y0.00.jpg"]
+OPTIMUM_IMAGE_PATH_LIST = ["/home/raspberry/projects/multireflection/data/real/x0.00_y0.00.jpg", "/home/raspberry/projects/multireflection/data/light/x0.00_y0.00.jpg", "/home/raspberry/projects/multireflection/data/new/x0.00_y0.00.jpg"]
 
 # EVAL
 EVAL_GRID_STEP = 0.5
